df579352dccaffe5efff40dd5d3f01529a099b68
Ev-riko <margiev.erik@list.ru>
2025-08-27 16:54:23 +0300

    Создал контроллер для страницы продукта. Настроил переходы между страницами. Сверстал страницу продукта.

---
+++ SuperHyperUltraAmazingGalaxyActionFigures/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHyperUltraAmazingGalaxyActionFigures.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        public IActionResult Index(Guid id)
+        {
+            var product = _productRepository.TryGetById(id);
+            if (product == null) 
+            {
+                //?
+            }
+            return View(product);
+        }
+    }
+}

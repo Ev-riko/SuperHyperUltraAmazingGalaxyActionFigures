830ef825af2cf53f94967c37cb707f3b56a47e42
Ev-riko <margiev.erik@list.ru>
2025-08-31 01:44:50 +0300

    Подправил верстку каталога

--- SuperHyperUltraAmazingGalaxyActionFigures/ProductsInMemoryRepository.cs
+++ SuperHyperUltraAmazingGalaxyActionFigures/ProductsInMemoryRepository.cs
@@ -10,6 +10,14 @@ namespace SuperHyperUltraAmazingGalaxyActionFigures
             new Product( "Монки Д Луффи", 3000000000, "Даже если весь мир говорит тебе остановиться, ты должен продолжать двигаться вперёд!", "/images/Luffy.png"),
             new Product( "Ророноа Зорро", 1111000000, "Только я могу назвать свою мечту глупой", "/images/Zoro.png"),
             new Product( "Киллер Би", 8, "Пока есть силы, ты сияешь, ты блестишь! Хачиби рулит тусовкой! О да!", "/images/KillerBee.png"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
+            new Product("Заглушка", 990, "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus.", "/images/plug.jpg"),
         };
 
         public List<Product> GetAll()

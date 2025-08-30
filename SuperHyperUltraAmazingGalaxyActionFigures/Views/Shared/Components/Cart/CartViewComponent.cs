53a1593e40331379f8db24ff3bae41bd67f8c0cb
Ev-riko <margiev.erik@list.ru>
2025-08-30 16:30:43 +0300

    Добавил отображение количества товаров в корзине с помощью компонентов

---
+++ SuperHyperUltraAmazingGalaxyActionFigures/Views/Shared/Components/Cart/CartViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHyperUltraAmazingGalaxyActionFigures.Views.Shared.ViewComponents.Cart
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public CartViewComponent(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var cart = _cartRepository.TryGetByUserId(Constants.UserId);
+            var cartItemsCount = cart?.Amount;
+            return View("Cart", cartItemsCount);
+        }
+    }
+}
